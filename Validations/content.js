f9cbe6e0516a4d9e113aa16104011050c798b8c9
Shreya Singh <95983195+Shreyasingh0405@users.noreply.github.com>
2024-09-10 16:49:17 +0530

    Update content.js validations

--- Validations/content.js
+++ Validations/content.js
@@ -10,14 +10,4 @@ const addContent = [
             return res.send({ status: 0, response: errors[0].msg })
         } return next()
     }]
-
-// const updateContent = [
-//     check('description').notEmpty().withMessage('description should be required'),
-//     check('title').notEmpty().withMessage('title should be required'),
-//     (req, res, next) => {
-//         const errors = validationResult(req).array()
-//         if (errors.length > 0) {
-//             return res.send({ status: 0, response: errors[0].msg })
-//         } return next()
-//     }]
 module.exports = { addContent }
